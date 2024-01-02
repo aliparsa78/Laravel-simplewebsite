ab5c0d14e00046e770c97c336a814bbc4d719cf6
aliparsa <aliparsa9@gmail.com>
2024-01-02 22:40:06 +0430

    footer copyright added

--- resources/views/Layout/app.blade.php
+++ resources/views/Layout/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Simple Website</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="custom.css">
 </head>
 <body>
     @include('Layout.navbar')
@@ -22,5 +23,9 @@
             </div>
         </div>
     </div>
+
+    <footer id="footer" class="text-center">
+        copyright &copy; 2024 aliparsa
+    </footer>
 </body>
 </html>
